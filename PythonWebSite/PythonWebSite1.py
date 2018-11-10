898884ff93587debe731e1327bea9529d92cafac
ihenisa <ihenisa1972@gmail.com>
2018-11-09 22:20:06 -0500

    Python WebSite Upates

--- PythonWebSite/PythonWebSite1.py
+++ PythonWebSite/PythonWebSite1.py
@@ -1,3 +1,12 @@
+# Ian Henisa
+# 11/09/18
 # Python Web Site File
 
 
+from flask import Flask app=Flask(__name__)@app.route('/')
+
+def home():
+    return "Website content goes here."
+
+if __name__=='__main__':
+    app.run(debug=True)
